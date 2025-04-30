14086f052a9e57dd985b69c51bd115cbad084a3d
Etries <elyudmichael@gmail.compub>
2025-04-30 19:24:41 -0400

    added functions for the ptins

--- modbus_client.py
+++ modbus_client.py
@@ -8,6 +8,7 @@ from pymodbus.mei_message import ReadDeviceInformationRequest
 
 #this is to chnage the output color to green and red
 GREEN = "\033[92m"
+RED = "\033[91m"
 RESET = "\033[0m"
 
 #prints menu
@@ -43,11 +44,50 @@ def parse_args():
     return args
 
 
-def display_modbus_info(ip, port):
-    client = ModbusTcpClient(host=ip, port=port)
-    if not client.connect():
-        print("Could not connect to Modbus server. Check IP/Port.")
-        sys.exit(1)
+
+def read_discrete_input(client):
+    print("Discrete Input Contacts [R/O]")
+    result = client.read_discrete_inputs(address=0, count=4, slave=1)
+    if result.isError():
+        print("Unable to read descreet input restart client or server")
+    else:
+        for i, val in enumerate(result.bits[:4]):
+            status = f"{GREEN}True{RESET}" if val else f"{RED}False{RESET}"
+            print(f"Contact {i}: {status}")
+    
+
+def read_discrete_output_coils(client):
+    print("Discrete Output Coils [R/W]")
+    result = client.read_coils(address=0, count=4, slave=1)
+    if result.isError():
+        print("Unable to read output coils. Restart client or server.")
+    else:
+        for i, val in enumerate(result.bits[:4]):
+            status = f"{GREEN}True{RESET}" if val else f"{RED}False{RESET}"
+            print(f"Coil {i}: {status}")
+
+def read_analog_input_registers(client):
+    print("Analogue Input Registers [R/O]")
+    result = client.read_input_registers(address=0, count=8, slave=1)
+    if result.isError():
+        print("Unable to read analogue input registers.")
+    else:
+        for i, val in enumerate(result.registers[:8]):
+            print(f"Input Register {i}: {val}")
+  
+
+def read_analog_output_registers(client):
+    print("Analogue Output Holding Registers [R/W]")
+    result = client.read_holding_registers(address=0, count=8, slave=1)
+    if result.isError():
+        print("Unable to read holding registers.")
+    else:
+        for i, val in enumerate(result.registers[:8]):
+            print(f"Holding Register {i}: {val}")
+
+
+def display_modbus_info(client):
+
 
     request = ReadDeviceInformationRequest()
     response = client.execute(request)
@@ -68,18 +108,32 @@ def display_modbus_info(ip, port):
 
 def main():
     args = parse_args()
-    display_modbus_info(args.ip, args.port)
+    client = ModbusTcpClient(host=args.ip, port=args.port)
+    if not client.connect():
+        print("Could not connect to Modbus server. Check IP/Port.")
+        sys.exit(1)
+    display_modbus_info(client)
     while True:  
         print_menu()
         choice = input("\nSelect an activity: ")
+        if choice == "1":
+            read_discrete_input(client)
+        elif choice == "2":
+            read_discrete_output_coils(client)   
+        elif choice == "3":
+            read_analog_input_registers(client)
+        elif choice == "4":
+            read_analog_output_registers(client)
 
-        if choice == "9":
+
+        elif choice == "9":
             print("\nTerminating client..../n")
             break
         elif choice not in [str(i) for i in range(1, 10)]:
             print("Not a valid choice, try again\n")
         else:
             print(f"You selected option {choice} n")
+        input("Main menu, press Enter: ")
 
 if __name__ == "__main__":
     main()
